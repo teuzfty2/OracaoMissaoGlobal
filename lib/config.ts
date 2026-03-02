2c703585bf4156f4fef0febf9f713837f219ba41
matheus <matheussfilipej@gmail.com>
2026-03-01 23:47:31 -0300

    att

---
+++ lib/config.ts
@@ -0,0 +1,3 @@
+// lib/config.ts
+
+export const DATABASE_URL="postgresql://postgres:Matchola8228..@db.nerfryuccuxfucrjckdp.supabase.co:5432/postgres"
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient();
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL, // pega a variável do Vercel
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production")
   globalForPrisma.prisma = prisma;
